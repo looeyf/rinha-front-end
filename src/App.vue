e2261c3a7cd7fec104e2af3d5f7c56ed1ec003cb
Luis Oliveira <luis10.pf@gmail.com>
2023-10-19 23:09:11 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import EmptyState from './components/EmptyState.vue';
+</script>
+
+<template>
+  <main>
+    <EmptyState />
+  </main>
+</template>
+
+<style scoped>
+main {
+  width: 100vw;
+  height: 100vh;
+  min-height: 100vh;
+}
+</style>
